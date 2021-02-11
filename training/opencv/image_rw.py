f5226de8f69a409ab021690d0f3486f9203e9ebc
byckillia891 <byckillia891@gmail.com>
2021-02-11 21:11:37 +0200

    chess

--- training/opencv/image_rw.py
+++ training/opencv/image_rw.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 27, 2021
-@author: Oleksandr Lytvynenko
-# read-write and show images with opencv
-"""
-
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,22 +7,14 @@ path = r"..\data\ResultImage.png"
 # read image
 img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 # img = np.ones((149, 894), dtype=np.uint8) * 128
-noise1 = np.random.normal(scale=12., size=(149, 894) )
-img1 = img + noise1
-
-noise2 = np.random.normal(scale=12., size=(149, 894) )
-img2 = img + noise2
-
-deltaimg = img1 - img2
-noise_std = (img1 - img2).std()
-print (noise_std)
-# show image with matplotlib.pyplot and cv2
-plt.imshow(img1, cmap='gray')
-plt.show()
-plt.imshow(deltaimg, cmap='gray', vmin=-25, vmax=25)
-plt.show()
-
-
-
-
-#
+i = 0
+j = 0
+while i < 149:
+    while j < 894:
+        if i > 0 and j > 0:
+            a = img[(i-1):(i+2):1 , (j-1):(j+2):1].reshape((9,1))
+            print(a)
+        else :
+            print("edge element")
+        i = i + 1
+        j = j + 1
--- training/processing_utils/circular_mask.py
+++ training/processing_utils/circular_mask.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 03, 2021
-@author: Oleksandr Lytvynenko
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 DEBUG = True
